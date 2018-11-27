c893eebb0e68ba5f9d083d03725221b7a22976ab
emile <emile.salem@workjam.com>
2018-11-26 23:19:27 -0500

    add producer

--- producer/index.js
+++ producer/index.js
@@ -2,6 +2,26 @@ module.exports = {
     start
 }
 
-function start (ch) {
+const {of} = require('rxjs')
 
+function start (ch) {
+    of('').pipe(
+        mergeMap(() => {
+            of('').pipe(
+                delay(randomDelay()),
+                tap(() => ch.publish('FCM.exchange', 'fcm', Buffer.from(JSON.parse(randomType()))))
+            )
+        }),
+        repeat()
+    )
+}
+
+function randomType(){
+    return {
+        type: `TYPE${Math.ceil(Math.random() * 3)}`
+    }
+}
+
+function randomDelay(){
+    return Math.floor(Math.random() * 1000)
 }
